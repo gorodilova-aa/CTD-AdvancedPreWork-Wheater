b7a0cbbf2494e134ef850dc8227c680cbae97419
gorodilova-aa <gorodilova.aa@gmail.com>
2026-05-12 11:30:18 -0400

    Update script.js
    - addecd a function to get weather icons 
    - updated view of current weather: icon and round the temperature

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ const btnForecast = document.getElementById('btn-forecast');
 const CARY = { name: "Cary, USA", lat: 35.79, lon: -78.78 };
 const NSK = { name: "Novosibirsk, RU", lat: 55.04, lon: 82.93 };
 
+
+// weather icons
+function getWeatherIcon(code) {
+    // Коды WMO: https://open-meteo.com/en/docs
+    if (code === 0) return '☀️';              // Clear sky
+    if (code >= 1 && code <= 3) return '🌤️';  // Partly cloudy
+    if (code >= 45 && code <= 48) return '🌫️'; // Fog
+    if (code >= 51 && code <= 67) return '🌧️'; // Rain/Drizzle
+    if (code >= 71 && code <= 77) return '❄️'; // Snow
+    if (code >= 80 && code <= 82) return '🌦️'; // Rain showers
+    if (code >= 95) return '⛈️';               // Thunderstorm
+    return '☁️';                               // Default
+}
+
+
 // current weather
 async function loadCurrentWeather() {
     container.innerHTML = "<p>Loading current weather...</p>";
@@ -28,11 +43,13 @@ async function loadCurrentWeather() {
         container.innerHTML = `
             <div class="card">
                 <h3>${CARY.name}</h3>
-                <p style="font-size: 24px;">${dataCary.current_weather.temperature}°C</p>    
+                <p style="font-size: 24px;">${getWeatherIcon(dataCary.current_weather.weathercode)}
+                ${Math.round(dataCary.current_weather.temperature)}°C</p>    
             </div>
             <div class="card">
                 <h3>${NSK.name}</h3>
-                <p style="font-size: 24px;">${dataNSK.current_weather.temperature}°C</p>   
+                <p style="font-size: 24px;">${getWeatherIcon(dataNSK.current_weather.weathercode)}
+                ${Math.round(dataNSK.current_weather.temperature)}°C</p>   
             </div>             
         `;
     } catch (error) {
